4f420572b8f15522eac62b1af947161dc020b049
Julio Acevedo <julio.acevedo@thincode.com>
2020-01-06 18:47:20 -0600

    Built for Heroku

---
+++ dist/js/details.e6127f83.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["details"],{"9f52":function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("h1",[e._v("This is a Details page")])])}],i=n("2877"),l={},r=Object(i["a"])(l,s,a,!1,null,null,null);t["default"]=r.exports}}]);
+//# sourceMappingURL=details.e6127f83.js.map
