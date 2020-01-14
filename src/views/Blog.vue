fbf27172745f9c7c4d938fe1153cd02ae34df37b
Julio Acevedo <julio.acevedo@thincode.com>
2020-01-13 18:42:43 -0600

    'En construcción' messages for MyBlog and SomeOfMyWork sections

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,44 @@
 <template>
-  <div class="blog">
-    <h1>This is a Blog page</h1>
-  </div>
+  <v-container
+    fluid
+    grid-list-md
+  >
+    <v-layout wrap class="mt-5" justify-center align-center>
+      <v-flex xs8>
+        <v-container fluid grid-list-md>
+          <v-layout row wrap>
+            <v-fex>
+
+        <v-card
+          class="mx-auto"
+          align-center="true"
+          elevation="24"
+        >
+          <v-img
+            class="white--text align-end"
+            height="500px"
+            max-width="800"
+            src="https://www.santacruzbicycles.com/files/styles/scb_natural_2000_auto/public/danny-inpinn.jpg?itok=_i-vHNYm"
+          >
+            <v-card-title>Coming soon...</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>Unfortunately, I'm not quite ready yet</div>
+
+            <div>But I hope this section will be ready soon...</div>
+          </v-card-text>
+
+          <v-card-actions>
+          </v-card-actions>
+        </v-card>
+
+            </v-fex>
+          </v-layout>
+        </v-container>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
